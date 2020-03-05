cfa21cbf601d5e20632d2e04f55e072d802ac74f
mdinn <mdinn@LAPTOP-IE3N3L18>
2020-03-04 19:11:03 -0500

    Added menu in client program with keys and values. Added hash table in server program with get and put functionality.

--- csis501programmingassignment-grossmeyer-weller/src/client.java
+++ csis501programmingassignment-grossmeyer-weller/src/client.java
@@ -7,14 +7,9 @@ public class client{
 	{
 		Scanner input = new Scanner(System.in);
 		String serverResponse;
-		double circleArea, circleRadius;
+		int choice;
+		String key, value;
 		
-		System.out.println("Enter the radius of the circle");
-		circleRadius = input.nextDouble();
-		
-		
-		
-		//System.out.println(add);
 		Socket socket = new Socket("localhost",9000);
 		
 		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);  /*  socket.getOutputStream() returns an output stream for this socket. 
@@ -22,17 +17,47 @@ public class client{
 																				This convenience constructor creates the necessary intermediateOutputStreamWriter, 
 																				which will convert characters into bytes using the default character encoding.*/
 
-		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));/* socket.getInputStream() returns an input stream for this socket. 
-																								  new InputStreamReader(socket.getInputStream()) creates an InputStreamReader that uses the default charset.
-																								  new BufferedReader(new InputStreamReader(socket.getInputStream())) creates a buffering character-input stream that uses a default-sized input buffer.
-																								*/
-				
-		out.println(circleRadius);
-		serverResponse = in.readLine();
-		circleArea = Double.valueOf(serverResponse);
-		System.out.println("The area of the circle with radius "+circleRadius+" is: "+ circleArea);
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));	/* 	socket.getInputStream() returns an input stream for this socket. 
+						  																			new InputStreamReader(socket.getInputStream()) creates an InputStreamReader that uses the default charset.																				 
+																									new BufferedReader(new InputStreamReader(socket.getInputStream())) creates a buffering character-input stream that uses a default-sized input buffer.*/	
 		
+		while(true)
+		{
+			System.out.println("Welcome to the DHT! Here are your choices:");
+			System.out.println("1-Insert a pair into DHT");
+			System.out.println("2-Get the IP address of content");
+			System.out.println("3-Exit");
+			System.out.println("Enter your choice:");
 			
+			choice = input.nextInt();
+		
+			switch(choice)
+			{
+				case 1:
+					System.out.println("Enter the content name:");
+					key = input.next();
+					System.out.println("Enter the IP address it is to be stored at:");
+					value = input.next();
+					out.println(choice);
+					out.println(key);
+					out.println(value);
+					System.out.println("Pair successfully inserted into DHT!");
+					System.out.println("Here is the new table: " + in.readLine() + "/n");
+					break;
+				case 2:
+					System.out.println("Enter the content name:");
+					key = input.next();
+					out.println(choice);
+					out.println(key);
+					value = in.readLine();
+					System.out.println("The IP address that the content is stored at is: " + value + "/n");
+					break;
+				case 3:
+					System.exit(0);
+				default:
+					System.out.println("Wrong Entry\n");
+			}
+		}
 		
 	}
 }
